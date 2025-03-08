7a1d8afe0325ed089bf6c66bc5293bc90037afea
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-03-08 14:10:43 +0530

    Add files via upload

---
+++ ExceptionHandling.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M1Preparation
+{
+    public class ExceptionHandling
+    {
+        public void Sample()
+        {
+            string input = null;
+            try
+            {
+                int size = input.Length;
+                Console.WriteLine(size);
+            }
+            catch(System.NullReferenceException ex)
+            {
+                Console.WriteLine("input string is null");
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception occured!!");
+            }
+            finally
+            {
+                Console.WriteLine("multiple catchs");
+            }
+        }
+    }
+}
